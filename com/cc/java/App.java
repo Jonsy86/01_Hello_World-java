edfe2c274378c46aa8917fc759992c2cd725b5c2
Jonsy86 <jonasnolte86@gmail.com>
2024-05-21 12:48:30 +0200

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,9 +3,11 @@ package com.cc.java;
 public class App 
 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+   public static void main(String[] args) {
+    System.out.println("Hello, World");
+
+
+   } 
     
 }
 
